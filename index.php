7c1b9f9f75aab0fe52e59bcfc3918747e8b9a5ab
Elaheh Jamali <42677561+Jamaliela@users.noreply.github.com>
2019-03-31 22:29:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Staff: Users</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Staff: Users">
+    <link rel="stylesheet" media="all" href="/red/public/styles.css">
+  </head>
+  <body>
+<div id="menu">
+  <ul>
+    <li><a href="/red/public/index.php">Public Site</a></li>
+    <li><a href="/red/public/staff/index.php">Staff Menu</a></li>
+    <li><a href="/red/public/staff/logout.php" style="color: red;">Log out</a></li>
+  </ul>
+</div>
+
+<div id="main-content">
+  <h1>Users</h1>
+
+  <a href="new.php">Add a User</a><br />
+  <br />
+
+  <table id="users" style="width: 500px;"><tr><th>First name</th><th>Last name</th><th>Username</th><th></th><th></th></tr><tr><td>Jimmy</td><td>Neutron</td><td>jmonroe99</td><td><a href="show.php?id=1">Show</a></td><td><a href="edit.php?id=1">Edit</a></td></tr><tr><td>Pat</td><td>Person</td><td>pperson</td><td><a href="show.php?id=3">Show</a></td><td><a href="edit.php?id=3">Edit</a></td></tr><tr><td>Bobby</td><td>Tables</td><td>lbtables</td><td><a href="show.php?id=2">Show</a></td><td><a href="edit.php?id=2">Edit</a></td></tr></table>
+</div>
+
+  </body>
+</html>
 

